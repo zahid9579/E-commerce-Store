74a9c925e4b60b7a113e01bfa767001cfa86594d
Zahid-9579 <zahidansari.er@gmail.com>
2025-04-17 21:41:18 +0530

    Shop Section and filter based on price and category done

---
+++ frontend/src/pages/Products/ProductCard.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {Link} from"react-router-dom"
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { useDispatch } from 'react-redux';
+import {addToCart} from "../../redux/features/cart/cartSlice"
+import {toast} from 'react-toastify'
+import HeartIcon from './HeartIcon'
+
+
+
+const ProductCard = ({p}) => {
+    const dispatch = useDispatch();
+
+
+    const addToCartHandler = (product, qty) => {
+      dispatch(addToCart({...product, qty}))
+      toast.success('Item added successfully')
+ 
+    }
+
+
+
+
+  return <div className='max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
+    <section className='relative'>
+        <Link to={`/product/${p._id}`}>
+        <span className='absolute bottom-3 right-3 bg-pink-100 text-pink-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded-full dark:bg-pink-900 dark:text-pink-300 '>{p?.brand}</span>
+
+        <img className="cursor-pointer" src={p.image} alt={p.name} style={{height:'170px', objectFit: "cover" }}/>
+        
+        </Link>
+        <HeartIcon product={p}/>
+    </section>
+
+    <div className='p-5'>
+      <div className='flex justify-between'>
+        <h5 className='mb-2 text-xl text-white dark:text-white'>{p?.name}</h5>
+
+        <p className="text-pink-500 font-semibold">{p?.price?.toLocaleString('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        })}</p>
+      </div>
+
+      <p className='mb-3 font-normal text-[#CFCFCF]'>
+        {p?.description?. substring(0, 60)} ...
+      </p>
+
+      <section className='flex justify-between items-center'>
+        <Link to={`/product/${p._id}`}
+        className='inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-700 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800'
+        >
+          Read more
+        </Link>
+
+        <button className='p-2 rounded-lg text-white cursor-pointer' onClick={() => addToCartHandler(p, 1)}>
+          <AiOutlineShoppingCart size={25}/>
+        </button>
+
+
+      </section>
+
+
+
+    </div>
+  </div>
+}
+
+export default ProductCard;
