7746b69e6fdb029771fe9fdd0c7a4ca0184606c9
Zahid-9579 <zahidansari.er@gmail.com>
2025-04-25 21:10:18 +0530

    PayPal Integration stated for order place

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -4,4 +4,5 @@ export const CATEGORY_URL = "/api/category";
 export const PRODUCT_URL = '/api/products';
 export const UPLOAD_URL = '/api/uploads';
 export const ORDER_URL = '/api/orders';
+// For PayMent 
 export const PAYPAL_URL = "/api/config/paypal";
